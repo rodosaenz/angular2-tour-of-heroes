47bd2a43b0dfb283d38979327813aec349db021c
Rodolfo Saenz <rodolfo.saenz@quanticvision.com>
2016-09-06 02:19:51 -0500

    3rd commit

---
+++ app/heroes.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { OnInit }    from '@angular/core';
+import { Router }    from '@angular/router';
+
+import { Hero }         from './hero';
+import { HeroService }  from './hero.service';
+
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: 'app/heroes.component.html',
+  styleUrls: ['app/heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  title  = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
