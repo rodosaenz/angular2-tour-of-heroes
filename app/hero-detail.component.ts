ebdda0317634870a4ed48da481a58b2ab297629f
Rodolfo Saenz <rodolfo.saenz@quanticvision.com>
2016-09-19 23:22:00 -0500

    Last tutorial step HTTP

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -7,17 +7,7 @@ import { Hero } from './hero';
 
 @Component({
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-      <button (click)="goBack()">Back</button>
-    </div>
-  `
+  templateUrl: 'app/hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit {
   @Input()
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero }   from './hero';
 import { HEROES } from './mock-heroes';
@@ -6,8 +9,16 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
 
+	private heroesUrl = 'app/heroes';  // URL to web api
+	private headers = new Headers({'Content-Type': 'application/json'});
+
+	constructor(private http: Http) { }
+
 	getHeroes(): Promise<Hero[]> {
-	  return Promise.resolve(HEROES);
+	  return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
 	}
 
 	getHeroesSlowly(): Promise<Hero[]> {
@@ -18,5 +29,35 @@ export class HeroService {
 	getHero(id: number): Promise<Hero> {
 	  return this.getHeroes()
 	             .then(heroes => heroes.find(hero => hero.id === id));
+	}
+
+	private handleError(error: any): Promise<any> {
+	  console.error('An error occurred', error); // for demo purposes only
+	  return Promise.reject(error.message || error);
+	}
+
+	update(hero: Hero): Promise<Hero> {
+	  const url = `${this.heroesUrl}/${hero.id}`;
+	  return this.http
+	    .put(url, JSON.stringify(hero), {headers: this.headers})
+	    .toPromise()
+	    .then(() => hero)
+	    .catch(this.handleError);
+	}
+
+	create(name: string): Promise<Hero> {
+	  return this.http
+	    .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+	    .toPromise()
+	    .then(res => res.json().data)
+	    .catch(this.handleError);
+	}
+
+	delete(id: number): Promise<void> {
+	  let url = `${this.heroesUrl}/${id}`;
+	  return this.http.delete(url, {headers: this.headers})
+	    .toPromise()
+	    .then(() => null)
+	    .catch(this.handleError);
 	}
 }
